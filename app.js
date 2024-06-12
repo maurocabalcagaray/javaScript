640537a10a1b24d71c1889882650f147d5ada3f4
mcabalcagaray <maurocabalcagaray@gmail.com>
2024-06-11 21:11:51 -0300

    clase-1

---
+++ app.js
@@ -0,0 +1,25 @@
+// VARIABLES y TIPOS DE DATOS
+
+let nombre = "mauro";
+let apellido = "cabalcagaray"; //---> String
+
+//alert( nombre );
+//alert( apellido );
+
+console.log(nombre);
+console.log(apellido);
+
+const documento = 39787917; //---> Number
+
+nombre = "javier";
+
+console.log(nombre);
+
+// documento = "1111111" // Esto no se puede
+
+let edadDeLaPersona = 31;
+console.log(edadDeLaPersona);
+
+let x = prompt("por favor ingrese un numero");
+
+console.log(x);
